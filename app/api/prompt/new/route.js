3d481f68a88a303a8e0890082796796395f4c4eb
x_yue <103266340+yuepem@users.noreply.github.com>
2024-07-04 14:11:47 +0200

    create new prompt and save to database done

---
+++ app/api/prompt/new/route.js
@@ -0,0 +1,22 @@
+import { connectToDB } from "@utils/database";
+import Prompt from "@models/prompt";
+
+export const POST = async (req, res) => {
+    const { userId, prompt, tag } = await req.json();
+
+    try {
+        await connectToDB();
+        const newPrompt = await Prompt.create({
+            creator: userId,
+            prompt,
+            tag
+        })
+        await newPrompt.save();
+        return new Response(JSON.stringify(newPrompt), { status: 201 },"Prompt created successfully");
+
+    } catch (error) {
+        return new Response("Failed to create a new prompt", { status: 500 });
+    }
+
+
+}
